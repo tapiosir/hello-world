e7bafaa4761d189512ac7a1f2c0fbcbc4e439727
Tapiosir <tapio.sirvio11@gmail.com>
2017-09-12 10:55:57 +0300

    updated hello world example

--- hello-world/hello-world/Program.cs
+++ hello-world/hello-world/Program.cs
@@ -7,7 +7,7 @@ namespace hello_world
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
